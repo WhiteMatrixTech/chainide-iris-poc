8a075a172e4d3d53dd34f44486e2cd9866418e2e
caoxmm <386707656@qq.com>
2023-09-14 11:19:36 +0800

    add mint& query poc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
-import { init, transfer } from "@/components/wallet";
+import {
+  doMintToken,
+  doQueryToken,
+  init,
+  mintToken,
+  transfer,
+} from "@/components/wallet";
 import Image from "next/image";
 import { useEffect } from "react";
 
@@ -10,32 +16,34 @@ export default function Home() {
     };
   }, []);
 
-  const transferValue = async () => {
-    const recipientInput = document.getElementById(
-      "recipient"
-    ) as HTMLInputElement;
+  const mintToken = async () => {
+    const symbolInput = document.getElementById("symbol") as HTMLInputElement;
     const amountInput = document.getElementById("amount") as HTMLInputElement;
+    const toInput = document.getElementById("to") as HTMLInputElement;
 
-    const recipient = recipientInput.value;
+    const symbol = symbolInput.value;
     const amount = amountInput.value;
-
-    // console.log(recipient, amount);
-
-    await transfer(recipient, amount);
+    const to = toInput.value;
+    await doMintToken(symbol, amount, to);
+  };
+  const queryToken = async () => {
+    await doQueryToken("testiristoken");
+  };
+  const queryUnExistToken = async () => {
+    await doQueryToken("testiristoken2323");
   };
   return (
     <main className="min-h-screen  p-24">
       <div>
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Recipient
+          Symbol
         </label>
         <input
           type="text"
-          id="recipient"
+          id="symbol"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="iaa1xt487wnadzuylgqg5ueajd3ahecg9r0mdt7pxs"
           required
-          value={"iaa1xt487wnadzuylgqg5ueajd3ahecg9r0mdt7pxs"}
+          value={"testiristoken"}
         />
       </div>
       <br />
@@ -47,17 +55,46 @@ export default function Home() {
           type="text"
           id="amount"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="0.1"
           required
           value={"0.1"}
         />
       </div>
       <br />
+      <div>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          To
+        </label>
+        <input
+          type="text"
+          id="to"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+          value={"iaa1xt487wnadzuylgqg5ueajd3ahecg9r0mdt7pxs"}
+        />
+      </div>
+      <br />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={transferValue}
+        onClick={mintToken}
       >
-        transfer
+        mintToken
+      </button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={queryToken}
+      >
+        成功的 query
+      </button>
+      &nbsp; &nbsp;
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={queryUnExistToken}
+      >
+        无法捕捉的 query
       </button>
     </main>
   );
--- src/components/wallet.ts
+++ src/components/wallet.ts
@@ -2,6 +2,22 @@ import * as iris from "@irisnet/irishub-sdk";
 import { Wallet } from "@irisnet/irishub-sdk/dist/src/types";
 import long from "long";
 
+interface IIrisAccountInfo {
+  account: {
+    typeUrl: string;
+    value: {
+      accountNumber: number;
+      address: string;
+      pubKey: {
+        typeUrl: string;
+        value: {
+          key: string;
+        };
+      };
+      sequence: number;
+    };
+  };
+}
 const chainId = "nyancat-9";
 export const init = async () => {
   if (!window.getOfflineSigner || !window.keplr) {
@@ -17,10 +33,10 @@ export const init = async () => {
           stakeCurrency: {
             coinDenom: "NYAN",
             coinMinimalDenom: "unyan",
-            coinDecimals: 6
+            coinDecimals: 6,
           },
           bip44: {
-            coinType: 118
+            coinType: 118,
           },
           bech32Config: {
             bech32PrefixAccAddr: "iaa",
@@ -28,22 +44,22 @@ export const init = async () => {
             bech32PrefixValAddr: "iaavaloper",
             bech32PrefixValPub: "iaavaloperpub",
             bech32PrefixConsAddr: "iaavalcons",
-            bech32PrefixConsPub: "iaavalconspub"
+            bech32PrefixConsPub: "iaavalconspub",
           },
           currencies: [
             {
               coinDenom: "NYAN",
               coinMinimalDenom: "unyan",
-              coinDecimals: 6
-            }
+              coinDecimals: 6,
+            },
           ],
           feeCurrencies: [
             {
               coinDenom: "NYAN",
               coinMinimalDenom: "unyan",
-              coinDecimals: 6
-            }
-          ]
+              coinDecimals: 6,
+            },
+          ],
         });
       } catch (e) {
         alert("Failed to suggest the chain");
@@ -56,21 +72,56 @@ export const init = async () => {
   await window.keplr?.enable(chainId);
 };
 
-export const transfer = async (recipient: string, priAmount: string) => {
-  let config = {
-    node: "https://rpc.nyancat.rainbow.one",
-    chainId: chainId
-  };
+export async function doQueryToken(symbol: string) {
+  const client = getClient();
 
-  const client = iris
-    .newClient(config)
-    .withKeyDAO({
-      write: () => {},
-      read: () => {
-        return "" as unknown as Wallet;
-      }
-    })
-    .withRpcConfig({ timeout: 90000 });
+  // client.token
+  //   .queryToken(symbol)
+  //   .then((res) => {
+  //     console.log("in success");
+  //     console.log(res);
+  //   })
+  //   .catch((e) => {
+  //     console.log("in error");
+  //     console.log(e);
+  //   });
+
+  try {
+    const res = await client.token.queryToken(symbol);
+    console.log("in success");
+    console.log(res);
+  } catch (e) {
+    console.log("in error");
+    console.error(e);
+  }
+}
+
+export const doMintToken = async (
+  symbol: string,
+  priAmount: string,
+  to?: string
+) => {
+  let amount = parseFloat(priAmount);
+  amount *= 1000000;
+  amount = Math.floor(amount);
+  const token = { symbol, amount, to };
+  const client = getClient();
+  const account = await window.keplr?.getKey(chainId)!;
+  const msgs: any[] = [
+    {
+      type: iris.types.TxType.MsgMintToken,
+      value: Object.assign({ owner: account.bech32Address }, token),
+    },
+  ];
+  console.log(msgs);
+
+  let tx_o = await signMessage(client, account, msgs);
+  let res = await client.tx.broadcast(tx_o, iris.types.BroadcastMode.Commit);
+  console.log("res:", res);
+};
+
+export const transfer = async (recipient: string, priAmount: string) => {
+  const client = getClient();
 
   let amount = parseFloat(priAmount);
   if (isNaN(amount)) {
@@ -82,29 +133,54 @@ export const transfer = async (recipient: string, priAmount: string) => {
   amount = Math.floor(amount);
   await window.keplr?.enable(chainId);
   const account = await window.keplr?.getKey(chainId)!;
+
+  console.log(account);
   const msgs = [
     {
       type: iris.types.TxType.MsgSend,
       value: {
         from_address: account.bech32Address,
         to_address: recipient,
-        amount: [{ denom: "unyan", amount }]
-      }
-    }
+        amount: [{ denom: "unyan", amount }],
+      },
+    },
   ];
+
+  let tx_o = await signMessage(client, account, msgs);
+  let res = await client.tx.broadcast(tx_o, iris.types.BroadcastMode.Commit);
+  console.log("res:", res);
+
+  return false;
+};
+async function signMessage(
+  client: iris.Client,
+  account: any,
+  msgs: {
+    type: iris.types.TxType;
+    value: any;
+  }[]
+) {
+  const accountInfo = (await client.auth.queryAccount(
+    account.bech32Address
+  )) as IIrisAccountInfo;
   let bTx = {} as unknown as iris.types.BaseTx;
-  bTx.account_number = 5626;
-  bTx.sequence = 4;
+  bTx.account_number = accountInfo.account.value.accountNumber;
+  bTx.sequence = accountInfo.account.value.sequence;
+
+  console.log({ bTx });
 
   let tx_o = client.tx.buildTx(msgs, bTx);
-  tx_o.setPubKey(Buffer.from(account.pubKey).toString("hex"));
+  tx_o.setPubKey(
+    Buffer.from(account.pubKey).toString("hex"),
+    accountInfo.account.value.sequence
+  );
   let signDoc = tx_o.getSignDoc(bTx.account_number, chainId);
 
   let keplr_signDoc_obj = {
     bodyBytes: signDoc.getBodyBytes(),
     authInfoBytes: signDoc.getAuthInfoBytes(),
     chainId: chainId,
-    accountNumber: new long(signDoc.getAccountNumber())
+    accountNumber: new long(signDoc.getAccountNumber()),
   };
   let s = await window.keplr?.signDirect(
     keplr_signDoc_obj.chainId,
@@ -116,9 +192,23 @@ export const transfer = async (recipient: string, priAmount: string) => {
     s.signed.authInfoBytes
   );
   tx_o.body = iris.types.tx_tx_pb.TxBody.deserializeBinary(s.signed.bodyBytes);
+  return tx_o;
+}
 
-  let res = await client.tx.broadcast(tx_o, iris.types.BroadcastMode.Commit);
-  console.log("res:", res);
+function getClient() {
+  let config = {
+    node: "https://rpc.nyancat.rainbow.one",
+    chainId: chainId,
+  };
 
-  return false;
-};
+  const client = iris
+    .newClient(config)
+    .withKeyDAO({
+      write: () => {},
+      read: () => {
+        return "" as unknown as Wallet;
+      },
+    })
+    .withRpcConfig({ timeout: 90000 });
+  return client;
+}
